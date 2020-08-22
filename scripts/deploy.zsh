52247ce31d2f8b76c323692ba2e958c45036cb4d
Ryan O'Shea <ryan@ryanoshea.com>
2020-08-22 12:33:07 -0400

    Remove custom deploy script

--- scripts/deploy.zsh
+++
@@ -1,9 +0,0 @@
-#!/bin/zsh
-
-yarn build
-
-ssh pi-crust "pkill -9 node"
-
-rsync -avz -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" --progress build/ pi-crust.local:/home/ryan/Serve/tess-bday-2020 --delete
-
-ssh pi-crust "nohup /home/ryan/.yarn/bin/serve -s /home/ryan/Serve/tess-bday-2020 > /dev/null 2>&1 &"
